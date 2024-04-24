b3ecd8f24ababf4572bb99ea6109ab842e7847c3
Smart developer <developer@yandex.ru>
2024-04-24 21:51:40 +0500

    make func, which makes profile

--- api_functions.py
+++ api_functions.py
@@ -1,7 +1,6 @@
 import logging
 
-from telegram import ReplyKeyboardRemove, InlineKeyboardButton, \
-    InlineKeyboardMarkup, ReplyKeyboardMarkup, Update
+from telegram import ReplyKeyboardRemove, ReplyKeyboardMarkup, Update
 from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, filters, ContextTypes
 
 logging.basicConfig(
@@ -26,6 +25,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_html("<b>🠖</b> /new - <i>создать или обновить свою анкету</i>\n"
+                                    "   <b>▼</b>\n"
+                                    "   <b>🠖</b> /cancel - <i>отменить создание анкеты</i>\n"
                                     "<b>🠖</b> /view - <i>перейти к поиску</i>\n"
                                     "<b>🠖</b> /stop - <i>!редакнуть!</i>\n"
                                     )
@@ -41,51 +42,115 @@ async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def new(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Как мне тебя называть ?")
-    return ASK_GAME
+    return ASK_NAME
 
 
 # --- Profile ---
 
+async def _name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
 
-async def _game(update, context):
+    with open('user_data.txt', 'a') as file:
+        file.write(user_message + '\n')
+
+    data = context.user_data
+    data['name'] = user_message
+
+    # ----SAVED!
+
     keyboard_games = [['CS2', 'Dota 2',
                        'Pubg', 'Roblox'], ['Fortnite', 'Valorant',
                                            'Minecraft', 'Terarria']]
     reply_markup = ReplyKeyboardMarkup(keyboard_games, one_time_keyboard=True, resize_keyboard=True)
     await update.message.reply_text("По какой игре ?", reply_markup=reply_markup)
-    return ASK_COOP
+
+    return ASK_GAME
 
 
-async def _coop(update, context):
+async def _game(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard_games = [['CS2', 'Dota 2',
+                       'Pubg', 'Roblox'], ['Fortnite', 'Valorant',
+                                           'Minecraft', 'Terarria']]
+
+    user_message = update.message.text
+
+    if user_message not in keyboard_games[0] and user_message not in keyboard_games[1]:
+        return ASK_GAME
+
+    with open('user_data.txt', 'a') as file:
+        file.write(user_message + '\n')
+
+    data = context.user_data
+    data['game'] = user_message
+    # ----SAVED!
     keyboard_games = [['Команду', 'Сокомадника']]
     reply_markup = ReplyKeyboardMarkup(keyboard_games, one_time_keyboard=True, resize_keyboard=True)
     await update.message.reply_text("Кого ищешь ?", reply_markup=reply_markup)
+
+    return ASK_COOP
+
+
+async def _coop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard_games = [['Команду', 'Сокомадника']]
+
+    user_message = update.message.text
+
+    if user_message not in keyboard_games[0]:
+        return ASK_COOP
+
+    with open('user_data.txt', 'a') as file:
+        file.write(user_message + '\n')
+
+    data = context.user_data
+    data['coop'] = user_message
+    # ----SAVED!
+    await update.message.reply_text("Сколько часов в твоей игре ?")
+
     return ASK_HOURS
 
 
-async def _hours(update, context):
-    await update.message.reply_text("Сколько часов в твоей игре ?")
+async def _hours(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
+
+    if not user_message.isdigit():
+        return ASK_HOURS
+
+    with open('user_data.txt', 'a') as file:
+        file.write(user_message + '\n')
+
+    data = context.user_data
+    data['hours'] = user_message
+    # ----SAVED!
+
+    await update.message.reply_text("Расскажи о себе и кого хочешь найти. Это поможет лучше подобрать тебе компанию.",
+                                    reply_markup=ReplyKeyboardRemove())
+
     return ASK_DESC
 
 
-async def _description(update, context):
-    await update.message.reply_text("Расскажи о себе и кого хочешь найти. Это поможет лучше подобрать тебе компанию.",
-                                    reply_markup=ReplyKeyboardRemove())
+async def _description(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
+
+    with open('user_data.txt', 'a') as file:
+        file.write(user_message + '\n')
+
+    data = context.user_data
+    data['desc'] = user_message
+    # ----SAVED!
+
     return ConversationHandler.END
 
 
 # ---------------- Handle ----------------
 
-def handle_text_message(update, context):
+async def handle_text_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type: str = update.message.chat.type
     text_received: str = update.message.text
-
-    # user_data = context.user_data
-    # if
+    await update.message.reply_text("-о-", )
 
 
-async def cancel(update, context):
-    update.message.reply_text("Диалог отменен.")
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Диалог отменен")
     return ConversationHandler.END
 
 
@@ -96,17 +161,19 @@ def main() -> None:
     app = Application.builder().token(TOKEN).build()
 
     # Connecting commands...
-    app.add_handler('start', start)
+    app.add_handler(CommandHandler('start', start))
     app.add_handler(CommandHandler("help", help_command))
-    app.add_handler('stop', stop)
-    app.add_handler('view', view)
+    app.add_handler(CommandHandler('stop', stop))
+    app.add_handler(CommandHandler('view', view))
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('new', new)],
         states={
-            ASK_GAME: [MessageHandler(~filters.ALL, _game)],
+            ASK_NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, _name)],
 
-            ASK_COOP: [MessageHandler(~filters.ALL, _coop)],
+            ASK_GAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, _game)],
+
+            ASK_COOP: [MessageHandler(filters.TEXT & ~filters.COMMAND, _coop)],
 
             ASK_HOURS: [MessageHandler(filters.TEXT & ~filters.COMMAND, _hours)],
 
@@ -117,5 +184,6 @@ def main() -> None:
     app.add_handler(conv_handler)
 
     # Connecting messages...
+    # app.add_handler(MessageHandler(filters.TEXT, handle_text_message)) - Этот обработчик реагирует на пользовательские сообщения, создан чисто для тестов
 
-    app.add_handler(handle_text_message)
+    app.run_polling()
--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 import logging
 
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import filters, ContextTypes, Application, ConversationHandler, CommandHandler, MessageHandler, \
-    CallbackQueryHandler
+from telegram import ReplyKeyboardRemove, InlineKeyboardButton, \
+    InlineKeyboardMarkup, ReplyKeyboardMarkup, Update
+from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, filters, ContextTypes
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
@@ -19,66 +19,47 @@ ASK_NAME, ASK_GAME, ASK_COOP, ASK_HOURS, ASK_DESC = range(1, 6)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
     await update.message.reply_html(
-        f'<b>Привет {user.mention_html()}! Я бот, который поможет найти команду или сокомадника. Cначала ознакомьтесь с командами ( /help ).</b>')
+        f"<b>Привет {user.mention_html()}! Я бот, который поможет найти команду или сокомадника."
+        f" Cначала ознакомьтесь с командами ( /help ).</b>",
+    )
 
 
-async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("soon..")
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_html("<b>🠖</b> /new - <i>создать или обновить свою анкету</i>\n"
+                                    "<b>🠖</b> /view - <i>перейти к поиску</i>\n"
+                                    "<b>🠖</b> /stop - <i>!редакнуть!</i>\n"
+                                    )
 
 
 async def view(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("soon..")
 
 
-async def new(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    games: list[list[InlineKeyboardButton]] = [
-        [
-         InlineKeyboardButton("CS2", callback_data=1),
-         InlineKeyboardButton("Dota 2", callback_data=2),
-         InlineKeyboardButton("Pubg", callback_data=3),
-         InlineKeyboardButton("Roblox", callback_data=4)
-        ],
-        [
-            InlineKeyboardButton("Fortnite", callback_data=5),
-            InlineKeyboardButton("Valorant", callback_data=6),
-            InlineKeyboardButton("Minecraft", callback_data=7),
-            InlineKeyboardButton("Terarria", callback_data=8)
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(games)
-    await update.message.reply_text("По какой игре ?", reply_markup=reply_markup)
-    return ASK_COOP
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("soon..")
+
+
+async def new(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Как мне тебя называть ?")
+    return ASK_GAME
 
 
 # --- Profile ---
 
+
 async def _game(update, context):
-    games = [
-        [
-            InlineKeyboardButton("CS2", callback_data=1),
-            InlineKeyboardButton("Dota 2", callback_data=2),
-            InlineKeyboardButton("Pubg", callback_data=3),
-            InlineKeyboardButton("Roblox", callback_data=4)
-        ],
-        [
-            InlineKeyboardButton("Fortnite", callback_data=5),
-            InlineKeyboardButton("Valorant", callback_data=6),
-            InlineKeyboardButton("Minecraft", callback_data=7),
-            InlineKeyboardButton("Terarria", callback_data=8)
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(games)
+    keyboard_games = [['CS2', 'Dota 2',
+                       'Pubg', 'Roblox'], ['Fortnite', 'Valorant',
+                                           'Minecraft', 'Terarria']]
+    reply_markup = ReplyKeyboardMarkup(keyboard_games, one_time_keyboard=True, resize_keyboard=True)
     await update.message.reply_text("По какой игре ?", reply_markup=reply_markup)
-    query = update.callback_query
-    query.answer()
     return ASK_COOP
 
 
 async def _coop(update, context):
-    r_coop = [InlineKeyboardButton('Сокомадника', callback_data='solo'),
-              InlineKeyboardButton('Команду', callback_data='team')]
-    markup = InlineKeyboardMarkup([r_coop])
-    await update.message.reply_text("Кого ищешь ?", reply_markup=markup)
+    keyboard_games = [['Команду', 'Сокомадника']]
+    reply_markup = ReplyKeyboardMarkup(keyboard_games, one_time_keyboard=True, resize_keyboard=True)
+    await update.message.reply_text("Кого ищешь ?", reply_markup=reply_markup)
     return ASK_HOURS
 
 
@@ -88,8 +69,19 @@ async def _hours(update, context):
 
 
 async def _description(update, context):
-    await update.message.reply_text("Расскажи о себе и кого хочешь найти. Это поможет лучше подобрать тебе компанию.")
+    await update.message.reply_text("Расскажи о себе и кого хочешь найти. Это поможет лучше подобрать тебе компанию.",
+                                    reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
+
+
+# ---------------- Handle ----------------
+
+async def handle_text_message(update, context):
+    message_type: str = update.message.chat.type
+    text_received: str = update.message.text
+
+    # user_data = context.user_data
+    # if
 
 
 async def cancel(update, context):
@@ -97,33 +89,17 @@ async def cancel(update, context):
     return ConversationHandler.END
 
 
-# ---------------- Responses ----------------
-
-# def hundle_response(text: str):
-#     pass
+# ---------------- Main functions ----------------
 
 
-# Обработчик коллбэк-запросов после ASK_COOP
-async def handle_callback(update, context):
-    query = update.callback_query
-    chosen_game = query.data
-
-    # Сохраняем выбор пользователя или выполняем другую логику, если это необходимо
-    user_data = context.user_data
-    user_data['chosen_game'] = chosen_game
-    await query.edit_message_text('Игра была добавлена.')
-    # Продолжаем диалог или переходим к следующему шагу
-    # В зависимости от логики вашего приложения
-
-
-def main():
+def main() -> None:
     app = Application.builder().token(TOKEN).build()
 
     # Connecting commands...
-    app.add_handler('start', start)
+    app.add_handler(CommandHandler('start', start))
     app.add_handler(CommandHandler("help", help_command))
-    app.add_handler('stop', stop)
-    app.add_handler('view', view)
+    app.add_handler(CommandHandler('stop', stop))
+    app.add_handler(CommandHandler('view', view))
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('new', new)],
@@ -140,7 +116,8 @@ def main():
     )
     app.add_handler(conv_handler)
 
-    # Connecting messages...
+    # Hundle messages...
 
-    # Connecting callback...
-    app.add_handler(CallbackQueryHandler(handle_callback))
+    app.add_handler(MessageHandler(filters.TEXT, handle_text_message))
+
+    app.run_polling()
