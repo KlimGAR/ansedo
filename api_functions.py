dee07f9d45834eda4cc2c8b400743a701a057f3a
Smart developer <developer@yandex.ru>
2024-04-26 12:10:30 +0500

    add input data and output data to database

--- api_functions.py
+++ api_functions.py
@@ -1,4 +1,5 @@
 import logging
+import sqlite3
 
 from telegram import ReplyKeyboardRemove, ReplyKeyboardMarkup, Update
 from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, filters, ContextTypes
@@ -186,4 +187,18 @@ def main() -> None:
     # Connecting messages...
     # app.add_handler(MessageHandler(filters.TEXT, handle_text_message)) - Этот обработчик реагирует на пользовательские сообщения, создан чисто для тестов
 
-    app.run_polling()
+
+# ввод данных в бд!
+# con = sqlite3.connect("data.sqlite")
+# cur = con.cursor()
+# result = cur.execute(
+#     f"""INSERT INTO profile(game, name, st, hours, description)
+#     VALUES({data['game']}, {data['name']},{data['coop']}, {data['hours']}, {data['desc']})""").fetchall()
+# con.close()
+# # вывод данных из бд!
+# con = sqlite3.connect("data.sqlite")
+# cur = con.cursor()
+# result_0 = cur.execute(
+#     f"""SELECT game, name, st, hours, description, user_id FROM profile""").fetchall()
+# con.close()
+# app.run_polling()
